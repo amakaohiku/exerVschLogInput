1e68fb9f637506a35d9f67c8a29341fbe9638d68
amakaohiku <amakanza1@gmail.com>
2020-12-09 12:35:59 -0700

    done

---
+++ index.js
@@ -0,0 +1,5 @@
+function fnl() {
+    var ent = document.getElementById("text-input").value;
+    console.log(ent)
+    document.getElementById("paragraph").textContent="Here is your output:  "+ ent
+}
